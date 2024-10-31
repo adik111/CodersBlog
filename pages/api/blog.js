7a925fae136c7ed266a33df4b707ec80eaaaf1fb
adik111 <adityakamate1@gmail.com>
2024-10-31 18:58:07 +0530

    Added mongodb and error

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -2,7 +2,12 @@ import mongoose from 'mongoose';
 import Blog from '../../models/BlogPost'
 
 export default async function handler(req, res) {
+  try{
     await mongoose.connect("mongodb+srv://aditya:kamate13579@projects.fvwgu.mongodb.net/CodersBlog?retryWrites=true&w=majority&appName=Projects")
     const blogData = await Blog.find().limit(req.query.count);
       res.status(200).json(blogData);
+  }
+  catch(err){
+    res.status(500).json(err)
+  }
   }
